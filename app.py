519106ced49dd8742912eb226b99ce9a420d54fa
Johnson  Ekedum <johnekedum@gmail.com>
2019-04-08 11:23:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
 
-
-    
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+########### Set up the chart
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+bitterness = go.Bar(
+    x=beers,
+    y=[35, 60, 85, 75],
+    name='IBU',
+    marker={'color':'red'}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=[5.4, 7.1, 9.2, 4.3],
+    name='ABV',
+    marker={'color':'blue'}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = 'Beer Comparison'
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+########### Display the chart
+
+app = dash.Dash()
+server = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
+    html.H1('Flying Dog Beers'),
     dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+        id='flyingdog',
+        figure=beer_fig
+    )]
+)
 
 if __name__ == '__main__':
-    app.run_server()
+app.run_server()
+    
