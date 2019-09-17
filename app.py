0a4a727e31fc2cc009645ae664f681d51e74e171
John <johnekedum@gmail.com>
2019-09-17 04:57:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from knackpy import Knack
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = [
-      ['********', '*****']
+      ['welcome', 'knackit']
 ]
 
 
